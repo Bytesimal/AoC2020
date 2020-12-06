dcbafd57b79b78aa22ab854fc5cea11364a7574e
Rohan Mathew <robozmat@gmail.com>
2020-12-06 18:03:44 +0000

    solved d6p1

---
+++ day6/pz1.py
@@ -0,0 +1,20 @@
+with open("input.txt") as f:
+    groups = [g.split() for g in "".join(f.readlines()).split("\n\n")]
+
+
+def main():
+    tot = 0
+
+    for g in groups:
+        opt = set([])
+        for rsp in g:
+            for c in rsp:
+                opt.add(c)
+
+        tot += len(opt)
+
+    print(tot)
+
+
+if __name__ == "__main__":
+    main()
